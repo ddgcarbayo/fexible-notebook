2f9a2b69f301a10daa207af3b01ae79366024477
Raul Lopez <rldona@gmail.com>
2014-06-21 00:41:32 +0200

    js logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,19 @@
+var name_columns = ['peliculas', 'ideas', 'compras', 'musica', 'trabajo'];
+		name_columns_size = name_columns.length;
+
 (function() {
 
-	var actual_textarea;
+	for(var i=0; i < name_columns_size; i++) {
+		if(localStorage.getItem('' + name_columns[i])) {
+			$('#' + name_columns[i] + ' textarea').val(localStorage.getItem('' + name_columns[i]));
+		}
+	};
 
 	$canvas = $("#canvas");
 	$colums = $(".colum");
 	$textarea = $colums.find('textarea');
 
 	$colums.on('click', function() {
-
 		$this = $(this);
 
 		$this.prevAll().css('flex-grow', '1');
@@ -22,7 +28,18 @@
 		$this.find('textarea').css('width', '100%').show();
 		$this.nextAll().find('textarea').css('width', '0').hide();
 		$this.find('textarea').focus();
+	});
 
+	$colums.on('focusout', function() {
+		var mini_data = {};
+
+		var _name = $(this).attr('id');
+		var _description = $("#" + _name).find('textarea').val();
+
+		localStorage.setItem(_name, _description);
+
+		mini_data.id = _name;
+		mini_data.description = _description;
 	});
 
 })();
