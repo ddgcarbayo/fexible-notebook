593c68b23586a6af94f609c00fbbffc33ef95102
Raul Lopez <rldona@gmail.com>
2014-06-20 21:39:43 +0200

    js logic

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,25 @@
 	$colums = $(".colum");
 
 	$colums.on('click', function() {
+
 		$this = $(this);
-
-		color = $this.data("color");
-
-		console.log(color);
 
 		$this.prevAll().css('flex-grow', '1');
 		$this.css('flex-grow', '5');
 		$this.nextAll().css('flex-grow', '1');
 
+		$this.prevAll().find('.label').fadeTo(200,1);
+		$this.find('.label').fadeTo(200,0).hide();
+		$this.nextAll().find('.label').fadeTo(200,1);
+
+		$this.prevAll().find('textarea').css('width', '0').hide();
+		$this.find('textarea').css('width', '100%').show();
+		$this.nextAll().find('textarea').css('width', '0').hide();
+		$this.find('textarea').focus();
+
+
 	});
+
+	console.log($("#peliculas").val());
 
 })();
