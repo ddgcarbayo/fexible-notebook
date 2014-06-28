98033c6634fa929cbb9488d3949f34a166dc2f37
Raul Lopez <rldona@gmail.com>
2014-06-28 15:50:36 +0200

    js changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,82 +1,83 @@
-var flexNote = new FlexNote();
+(function(){
 
-function FlexNote() {
+	function FlexNote() {
 
-	var $canvas = document.querySelector("#canvas"),
-			$columns = $(".colum"),
-			$inputs = $(".colum input"),
-			$textarea = $columns.find('textarea');
+		var $canvas = document.querySelector("#canvas"),
+				$columns = $(".colum"),
+				$inputs = $(".colum input"),
+				$textarea = $columns.find('textarea');
 
-	function getDataStorage(num_colums) {
-		for(var i=0; i <= num_colums; i++) {
-			if(localStorage.getItem('c' + i)) {
-				el = $('.colum').data('column', 'c' + i)[i];
-				$(el).find('input').val(JSON.parse(localStorage.getItem('c' + i)).name);
-				$(el).find('textarea').val(JSON.parse(localStorage.getItem('c' + i)).description);
-			}
+		function getDataStorage(num_colums) {
+			for(var i=0; i <= num_colums; i++) {
+				if(localStorage.getItem('c' + i)) {
+					el = $('.colum').data('column', 'c' + i)[i];
+					$(el).find('input').val(JSON.parse(localStorage.getItem('c' + i)).name);
+					$(el).find('textarea').val(JSON.parse(localStorage.getItem('c' + i)).description);
+				}
+			};
+		}
+
+		function setDataStorage() {
+			$columns.on('focusout', function() {
+				var data = {},
+						pos_column = $(this).data("column"),
+						name = $(this).find('input').val(),
+						description = $(this).find('textarea').val();
+
+				data.name = name;
+				data.description = description;
+
+				localStorage.setItem(pos_column, JSON.stringify(data));
+			});
+		}
+
+		function animationColumns() {
+			$columns.on('click', function() {
+				el = $(this);
+
+				el.prevAll().css('width', '15%');
+				el.css('width', '40%');
+				el.nextAll().css('width', '15%');
+
+				el.prevAll().find('textarea').css('width', '0').hide();
+				el.find('textarea').css('width', '100%').fadeTo(250, 1).show();
+				el.nextAll().find('textarea').css('width', '0').hide();
+				//el.find('textarea').focus();
+
+				el.prevAll().find('.inner').css('height', 'auto');
+				el.find('.inner').css('height', '100%');
+				el.nextAll().find('.inner').css('height', 'auto');
+			});
+		}
+
+		this.init = function() {
+			$('#year').text(flexNote.getDate());
+
+			$inputs.fadeTo(1500, 1);
+
+			getDataStorage(5);
+			setDataStorage();
+
+			animationColumns();
 		};
+
 	}
 
-	function setDataStorage() {
-		$columns.on('focusout', function() {
-			var data = {},
-					pos_column = $(this).data("column"),
-					name = $(this).find('input').val(),
-					description = $(this).find('textarea').val();
+	FlexNote.prototype = {
 
-			data.name = name;
-			data.description = description;
+		constructor: FlexNote,
 
-			localStorage.setItem(pos_column, JSON.stringify(data));
-		});
-	}
+		getDate: function() {
+			var today = new Date();
+			return today.getFullYear();
+		}
 
-	function animationColumns() {
-		$columns.on('click', function() {
-			el = $(this);
-
-			el.prevAll().css('width', '15%');
-			el.css('width', '40%');
-			el.nextAll().css('width', '15%');
-
-			el.prevAll().find('textarea').css('width', '0').hide();
-			el.find('textarea').css('width', '100%').fadeTo(250, 1).show();
-			el.nextAll().find('textarea').css('width', '0').hide();
-			//el.find('textarea').focus();
-
-			el.prevAll().find('.inner').css('height', 'auto');
-			el.find('.inner').css('height', '100%');
-			el.nextAll().find('.inner').css('height', 'auto');
-		});
-	}
-
-	this.init = function() {
-		$('#year').text(flexNote.getDate);
-
-		$inputs.fadeTo(1500, 1);
-
-		getDataStorage(5);
-		setDataStorage();
-
-		animationColumns();
 	};
 
-}
 
-//FlexNote.prototype.constructor = FlexNote;
-
-FlexNote.prototype = {
-
-	getDate: function() {
-		console.log('flex');
-	}
-
-};
-
-document.addEventListener('DOMContentLoaded', function() {
+	var flexNote = new FlexNote();
 
 	flexNote.init();
 
-	console.log(flexNote.getDate());
 
-});
+})();
