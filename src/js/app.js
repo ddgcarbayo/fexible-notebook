dfb636b8ccf132d4daf68318980ecedffe1fb57e
Raul Lopez <rldona@gmail.com>
2014-11-23 22:00:13 +0100

    js change

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 $(function() {
 
+	// TODO: remove jQuery by JS Vanilla
+
 	$canvas = $("#canvas");
 	$colums = $(".colum");
 	$inputs = $(".colum input");
@@ -12,32 +14,12 @@ $(function() {
 	var COLUMNS = 5;
 
 	for(var i=0; i <= COLUMNS; i++) {
-
 		if(localStorage.getItem('c' + i)) {
-
 			JSON.parse(localStorage.getItem('c' + i));
-			//console.log(JSON.parse(localStorage.getItem('c' + i)));
-
 			var that = $('.colum').data('colum', 'c' + i)[i];
-
-			//console.log(that);
-
-			//console.log('name_input: ', $(that).find('input').val());
-
-
-
-			//console.log('name_storage: ', JSON.parse(localStorage.getItem('c' + i)));
-
-			//var aux_name = JSON.parse(localStorage.getItem('c' + i));
-
-			//console.log(aux_name.name);
-
 			$(that).find('input').val(JSON.parse(localStorage.getItem('c' + i)).name);
-
 			$(that).find('textarea').val(JSON.parse(localStorage.getItem('c' + i)).description);
-
 		}
-
 	};
 
 	$colums.on('click', function() {
@@ -51,7 +33,6 @@ $(function() {
 		$this.prevAll().find('textarea').css('width', '0').hide();
 		$this.find('textarea').css('width', '100%').fadeTo(250, 1).show();
 		$this.nextAll().find('textarea').css('width', '0').hide();
-		//$this.find('textarea').focus();
 
 		$this.prevAll().find('.inner').css('height', '0');
 		$this.find('.inner').css('height', '100%');
@@ -71,8 +52,6 @@ $(function() {
 		data.description = description;
 
 		localStorage.setItem(pos_column, JSON.stringify(data));
-
-		//console.log(localStorage.getItem('c0').name);
 
 	});
 
